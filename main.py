7220407cfa1109f7f3dcf24f8b2c906a16477fc5
Nazar Bezmenov <nazarbezmenov1@gmail.com>
2023-09-16 22:03:30 +0100

    Fix bug and clean code

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 def parse(query: str) -> dict:
     dic = {}
-    print(query)
-    arr = query.split("?")
-    if len(arr)==2:
-        query = arr[1]
-        print(query)
-        arr = query.split("&")
-        for par in arr:
-            pair = par.split("=")
-            print(pair)
-            if len(pair)==2:
-                if len(pair[0])>0 and len(pair[1])>0:
-                    dic[pair[0]]=pair[1]
-                    print(dic)
+    arr = query.split("?", 1)
+    query = arr[1] if len(arr)==2 else ''
+    arr = query.split("&")
+    for parameter in arr:
+        pair = parameter.split("=", 1)
+        if len(pair) == 2:
+            if len(pair[0]) > 0 and len(pair[1]) > 0:
+                dic[pair[0]] = pair[1]
     return dic
 
 
@@ -25,7 +20,7 @@ if __name__ == '__main__':
     assert parse('http://example.com/?name=') == {}
     assert parse('http://example.com/?name') == {}
     assert parse('http://example.com/?=Dima') == {}
-    assert parse('http://example.com/?name==Dima') == {}
-    assert parse('http://example.com/?name=Dima=Masha') == {}
+    assert parse('http://example.com/?name==Dima') == {'name': '=Dima'}
+    assert parse('http://example.com/?name=Dima=Masha') == {'name': 'Dima=Masha'}
     assert parse('http://example.com/?name=Dima&name=Masha') == {'name': 'Masha'}
     assert parse('http://example.com/?name=Dima&fuga=Dima') == {'name': 'Dima', 'fuga': 'Dima'}
